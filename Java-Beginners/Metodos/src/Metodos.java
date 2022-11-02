acda9c00d7b304a0248f34ab68a017793883e7ad
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-11-02 12:30:38 -0300

    comentário sobre boas práticas e um exemplo de método

---
+++ Java-Beginners/Metodos/src/Metodos.java
@@ -0,0 +1,18 @@
+public class Metodos {
+    public static void main(String[] args) throws Exception {
+        // classe = atributos + metodos (funções, subrotinas)
+        // critérios de nomeação: verbo no infinitivo, camelCase
+        // parametros necessarios
+        // metodo dentro da classe
+        //retorna valor ou nao? void 
+        // necessário se atentar a tratamento de erros, divisor não pode ser 0
+        // visivel por todo o sistema, privado, nao visivel para outras classes, herança
+    }
+
+    public int dividir(){ 
+        int a, b;
+        a = 1;
+        b = 4;
+        return a/b; 
+    }
+}
