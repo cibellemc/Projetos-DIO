bfe6afcaac104b1f4ce6688765fd592174e09e0b
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-11-02 14:24:07 -0300

    documentação, tags e comentários

---
+++ Java-Beginners/Terminal/src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // One line
+    
+        /* Comentario
+         * de 
+         * varias 
+         * linhas
+         */   
+    }
+
+     /** duas estrelas sao usadas para dar aspecto de documentação
+     */
+     public void metodo(){}
+
+     // lembrar que comentarios nao devem ser usados para mitigar erros de um codigo mal elaborado
+
+     /* para criar documentação formato html:
+      * no terminal execute o comando javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
+      */
+}
