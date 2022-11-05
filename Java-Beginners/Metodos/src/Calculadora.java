67c07eabbfd30f231582536a242c7709fad011fe
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-11-05 11:43:06 -0300

    removendo comentarios e criando readme

--- Java-Beginners/Metodos/src/Calculadora.java
+++ Java-Beginners/Metodos/src/Calculadora.java
@@ -1,20 +1,5 @@
 public class Calculadora {
-    /**
-     * classe = atributos + metodos (funções, subrotinas)
-     * visibilidade: todo o sistema (public), privado (private), invisivel para outras classes (protected)
-     * tipo de dado: concreto ou abstrato
-     * modificador: static ou final
-     * retorno: tipo de dado (primitivos: int, boolean) ou void 
-     * nome do método: camelCase
-     * parametros necessarios: por valor/cópia ou referência/endereço
-     * exceções: necessário se atentar a tratamento de erros (ex: divisor não pode ser 0) 
-     * corpo: código ou vazio (termina em;)
-     * assinatura = nome + parametros (ou só o abrir e fechar de parênteses
-     * construtor, destrutor: métodos especiais em OO
-     * mensagem: solicitar que o método execute
-     * os métodos só tem como obrigatorio o retorno e o nome
-     */
-
+    
     public int somar(int a, int b){ 
         return a+b; 
     }
---
+++ Java-Beginners/Metodos/src/README.md
@@ -0,0 +1,15 @@
+### Comentários sobre métodos:
+
+* classe = atributos + metodos (funções, subrotinas)
+* visibilidade: todo o sistema (public), privado (private), invisivel para outras classes (protected)
+* tipo de dado: concreto ou abstrato
+* modificador: static ou final
+* retorno: tipo de dado (primitivos: int, boolean) ou void 
+* nome do método: camelCase
+* parametros necessarios: por valor/cópia ou referência/endereço
+* exceções: necessário se atentar a tratamento de erros (ex: divisor não pode ser 0) 
+* corpo: código ou vazio (termina em;)
+* assinatura = nome + parametros (ou só o abrir e fechar de parênteses
+* construtor, destrutor: métodos especiais em OO
+* mensagem: solicitar que o método execute
+* os métodos só tem como obrigatorio o retorno e o nome
