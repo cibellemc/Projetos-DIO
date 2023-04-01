9e418e5032a81a627264d86b942ef344a50e6cea
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-04-01 16:35:24 -0300

    add: extraindo detalhes dos pokemons

--- angular/pokedex/assets/js/main.js
+++ angular/pokedex/assets/js/main.js
@@ -1,14 +1,19 @@
 
+function convertPokemonTypeToLi(pokemonTypes) {
+    // o dicionário é types:[{"slot": 1,"type": {"name": "grass","url": "https://pokeapi.co/api/v2/type/12/"},{"slot": 2,"type": {"name": "poison", "url": "https://pokeapi.co/api/v2/type/4/"}}]
+
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToList(pokemon){
     return `
     <li class="pokemon">
-    <span class="number">#001</span>
+    <span class="number">#${pokemon.id}</span>
     <span class="name">${pokemon.name}</span>
     
     <div class="detail">
         <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+            ${convertPokemonTypeToLi(pokemon.types).join('')}
         </ol>
 
         <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
@@ -19,6 +24,7 @@ function convertPokemonToList(pokemon){
 
 const pokemonsList = document.getElementById('pokemonsList')
 
+// busca a lista de pokemons e transforma poke em lista string de html e concatena lista sem separador, formando um novo html
 pokeapi.getPokemons().then((pokemons = []) => { 
     // em vez de usar for, estimula a página a renderizar re uma vez
     pokemonsList.innerHTML += pokemons.map(convertPokemonToList).join('')
--- angular/pokedex/assets/js/pokeapi.js
+++ angular/pokedex/assets/js/pokeapi.js
@@ -1,12 +1,18 @@
 
 const pokeapi = {}
 
+pokeapi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeapi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     // fetch retorna um promise - processamento assíncron: resposta não é imediata, se der certo uma hora você recebe
-    return fetch(url)
-    .then((response) =>  response.json()) // quando recurso for disponibilizado, converte pra json retorna uma promise dessa conversão
-    .then((json) => json.results) //segundo then recebe o resultado do primeiro - pega os resultados (array que nos interessa)
-    .catch((error) => console.log(error))
+    return fetch(url) // requisição pra url
+        .then((response) =>  response.json()) // quando recurso for disponibilizado, converte pra json retorna uma promise dessa conversão
+        .then((json) => json.results) //segundo then recebe o resultado do primeiro - pega os resultados (array que nos interessa)
+        .then((pokemons) => pokemons.map(pokeapi.getPokemonDetail)) // pra cada pokemon da lista faz uma requisição pro detalhe dele
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetail) => pokemonsDetail)
 }
