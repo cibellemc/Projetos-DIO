4a83f52475acefd61503b2a47fe063da7407e06e
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-04-13 20:37:09 -0300

    add: style no component e criação de novos cards

--- angular/componentes/components/CardNews.js
+++ angular/componentes/components/CardNews.js
@@ -17,7 +17,7 @@ class Cardnews extends HTMLElement {
 
         // poderia ter uma função para cada filho
         const cardRight = document.createElement("div")
-        cardRight.setAttribute("class", "card-right")
+        cardRight.setAttribute("class", "card_right")
 
         // set = atribui valor a um prop, get = pega o valor que foi passado
         const img = document.createElement("img")
@@ -29,16 +29,16 @@ class Cardnews extends HTMLElement {
 
         // parte da notícia em si
         const cardLeft = document.createElement("div")
-        cardLeft.setAttribute("class", "card-left")
+        cardLeft.setAttribute("class", "card_left")
 
         const author = document.createElement("span")
         // o texto do elemento é o conteúdo + o valor do atributo passado no html
         // no caso de não ser passado (null), usa o default
-        author.textContent = "By " + (this.getAttribute("author") || "By Anonimous")
+        author.textContent = "By " + (this.getAttribute("author") || "Anonimous")
 
         const newsTitle = document.createElement("a")
         newsTitle.textContent = this.getAttribute("newsTitle")
-        newsTitle.href = this.getAttribute("") // em vez de setar pode acessar direto
+        newsTitle.href = this.getAttribute("href") // em vez de setar pode acessar direto
 
         const description = document.createElement("p")
         description.textContent = this.getAttribute("description")
@@ -55,7 +55,46 @@ class Cardnews extends HTMLElement {
     }
 
     styles(){
-
+        const style = document.createElement("style")
+        style.textContent = `
+        .card {
+            width: 80%;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            -webkit-box-shadow: 2px 6px 13px -4px rgba(0,0,0,1);
+            -moz-box-shadow: 2px 6px 13px -4px rgba(0,0,0,1);
+            box-shadow: 2px 6px 13px -4px rgba(0,0,0,1);
+        }
+        
+        .card_left {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding: 10px;
+        }
+        
+        .card_left a{
+            font-size: 1.5rem;
+            font-weight: bold;
+            margin-top: 15px;
+            color: black;
+            text-decoration: none;
+        }
+        
+        .card_left span {
+            font-weight: bold;
+        }
+        
+        .card_left p {
+            color: gray;
+        }
+        
+        .card_right img {
+            width: 200px;
+        }        
+        `
+        return style
     }
 }
 
