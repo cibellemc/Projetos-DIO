79b082b249ed9f9d0686962589cea4effc381fc1
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-04-13 19:41:34 -0300

    add: get e set de prop

--- angular/componentes/components/CardNews.js
+++ angular/componentes/components/CardNews.js
@@ -1,9 +1,13 @@
-class Cardnews extends HTMLElement { //herda características de um elemento
+class Cardnews extends HTMLElement { 
     constructor(){
+        //herda características de um elemento html
         super()
 
+        // cria um shadow
         const shadow = this.attachShadow({mode: "open"}) //pode ser influenciado pelo html externo
+        // link com os elementos construídos
         shadow.appendChild(this.build())
+        // link com o css
         shadow.appendChild(this.styles())
     }
 
@@ -15,20 +19,31 @@ class Cardnews extends HTMLElement { //herda características de um elemento
         const cardRight = document.createElement("div")
         cardRight.setAttribute("class", "card-right")
 
+        // set = atribui valor a um prop, get = pega o valor que foi passado
         const img = document.createElement("img")
+        img.setAttribute("src", "assets/1002903_1.png")
+
         cardRight.appendChild(img)
+
 
         // parte da notícia em si
         const cardLeft = document.createElement("div")
         cardLeft.setAttribute("class", "card-left")
 
-        const span = document.createElement("span")
-        const a = document.createElement("a")
-        const p = document.createElement("p")
+        const author = document.createElement("span")
+        // o texto do elemento é o conteúdo + o valor do atributo passado no html
+        author.textContent = "By " + this.getAttribute("author")
 
-        cardLeft.appendChild(span)
-        cardLeft.appendChild(a)
-        cardLeft.appendChild(p)
+        const newsTitle = document.createElement("a")
+        newsTitle.textContent = this.getAttribute("newsTitle")
+
+        const description = document.createElement("p")
+        description.textContent = this.getAttribute("description")
+
+
+        cardLeft.appendChild(author)
+        cardLeft.appendChild(newsTitle)
+        cardLeft.appendChild(description)
 
         // "pendura no componente principal"
         componentRoot.appendChild(cardLeft)
