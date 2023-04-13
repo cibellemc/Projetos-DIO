e6c2c2ad50778c855c67393320a749da915fed55
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-04-13 19:25:16 -0300

    add: shadow bloco de notícias

--- angular/componentes/components/CardNews.js
+++ angular/componentes/components/CardNews.js
@@ -3,7 +3,40 @@ class Cardnews extends HTMLElement { //herda características de um elemento
         super()
 
         const shadow = this.attachShadow({mode: "open"}) //pode ser influenciado pelo html externo
-        shadow.innerHTML = "<h1>hello</h1>"
+        shadow.appendChild(this.build())
+        shadow.appendChild(this.styles())
+    }
+
+    build(){
+        const componentRoot = document.createElement("div")
+        componentRoot.setAttribute("class", "card")
+
+        const cardRight = document.createElement("div")
+        cardRight.setAttribute("class", "card-right")
+
+        const img = document.createElement("img")
+        cardRight.appendChild(img)
+
+        const cardLeft = document.createElement("div")
+        cardLeft.setAttribute("class", "card-left")
+
+        const span = document.createElement("span")
+        const a = document.createElement("a")
+        const p = document.createElement("p")
+
+        cardLeft.appendChild(span)
+        cardLeft.appendChild(a)
+        cardLeft.appendChild(p)
+
+        // "pendura no componente principal"
+        componentRoot.appendChild(cardLeft)
+        componentRoot.appendChild(cardRight)
+
+        return componentRoot
+    }
+
+    styles(){
+
     }
 }
 
