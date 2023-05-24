09f2cc84500e0d238c98bf0cca58d64850275a05
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-05-23 21:40:54 -0300

    add: more notices

--- angular/blog/src/app/data/dataFake.ts
+++ angular/blog/src/app/data/dataFake.ts
@@ -4,5 +4,26 @@ export const dataFake = [
         "title": "Novo curso Angular",
         "description": "Playlist disponível no Youtube",
         "photoCover": "https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/1275957/retina_1708x683_cover-top-18-most-common-angularjs-developer-mistakes-41f9ad303a51db70e4a5204e101e7414.png"
+    },
+
+    {
+        "id": "2",
+        "title": "MCTI lança programa Hackers do Bem para capacitar profissionais em cibersegurança",
+        "description": "Programa receberá R$ 32,6 milhões em investimentos e será feito em parceria com a RNP e o Senai",
+        "photoCover": "https://www.gov.br/mcti/pt-br/acompanhe-o-mcti/noticias/2023/05/mcti-lanca-programa-hackers-do-bem-para-capacitar-profissionais-em-ciberseguranca/original-18fa8f61ed2faa7c4871e2e0f7ee1997.png/@@images/01a79e33-3dec-4529-809d-c1f91d571a5e.png"
+    },
+
+    {
+        "id": "3",
+        "title": "Oportunidades de formação tecnológica no Ceará",
+        "description": "Diversas iniciativas públicas e privadas oferecem formações tecnológicas, com o intuito de ampliar oportunidades e fomentar a demanda das empresas.",
+        "photoCover": "https://www.opovo.com.br/_midias/jpg/2023/05/19/818x460/1_cjovem_divulgacao_o_povo-22033537.jpeg?20230519185559?20230519185559"
+    },
+
+    {
+        "id": "4",
+        "title": "Queda global do Instagram foi causada por falha técnica, diz Meta",
+        "description": "Após o serviço apresentar instabilidades e ficar fora do ar para diversos usuários globalmente, a empresa lançou um comunicado quando o aplicativo voltou a funcionar.",
+        "photoCover": "https://tm.ibxk.com.br/2023/05/22/22094128935038.jpg?ims=1120x420"
     }
 ]
