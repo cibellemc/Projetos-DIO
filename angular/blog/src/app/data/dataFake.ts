fc22357680d815eab29c589501cd82bc3981e835
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-05-23 21:28:47 -0300

    add: blog

---
+++ angular/blog/src/app/data/dataFake.ts
@@ -0,0 +1,8 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Novo curso Angular",
+        "description": "Playlist disponível no Youtube",
+        "photoCover": "https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/1275957/retina_1708x683_cover-top-18-most-common-angularjs-developer-mistakes-41f9ad303a51db70e4a5204e101e7414.png"
+    }
+]
