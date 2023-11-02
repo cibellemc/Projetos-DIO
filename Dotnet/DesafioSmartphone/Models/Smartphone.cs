90c2d84b7919eecd84aef266f79a253db44b249c
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-11-01 23:39:31 -0300

    add: desafio do smartphone - poo

---
+++ Dotnet/DesafioSmartphone/Models/Smartphone.cs
@@ -0,0 +1,37 @@
+namespace DesafioSmartphone.Models
+{
+    public abstract class Smartphone
+    {
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+         public string Numero { get; set; }
+         private string Modelo { get; set; }
+         private string IMEI { get; set; }
+         private int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Dotnet/DesafioSmartphone/Program.cs
@@ -0,0 +1,7 @@
+﻿using DesafioSmartphone.Models;
+
+Iphone x = new("999", "xre", "123-5", 32);
+x.InstalarAplicativo("zap");
+
+Nokia y = new("998", "yre", "y123-5", 64);
+y.InstalarAplicativo("télegram");
