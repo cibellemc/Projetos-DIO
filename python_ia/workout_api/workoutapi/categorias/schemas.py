befcafc6a70d0f64f8063649c36b4f7a764a33c5
Cibelle Maciel <cibellemaciel02@gmail.com>
2024-07-09 21:00:58 -0300

    feat: criação de categorias/centro de treinamento e relacionamentos com atleta

---
+++ python_ia/workout_api/workoutapi/categorias/schemas.py
@@ -0,0 +1,8 @@
+from typing import Annotated
+
+from pydantic import Field
+from contrib.schemas import BaseSchema
+
+
+class CategoriaIn(BaseSchema):
+    nome: Annotated[str, Field(description='Nome da categoria', example='Scale', max_length=10)]
